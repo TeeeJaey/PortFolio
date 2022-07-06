a931b69b84f400097ee784d56bd66a390ef9163e
Tejas Jadhav <tjadhav95@gmail.com>
2022-07-06 11:55:44 +0530

    initial commit

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -3,21 +3,21 @@ import "./Skills.css";
 import { menuBarIndex } from "../../Constants";
 
 export default function Skills({ selectedTab }) {
-    useEffect(() => {
-        if (selectedTab !== menuBarIndex.Skills) {
-            document.getElementById("skills").classList.add("off");
-        } else {
-            document.getElementById("skills").classList.remove("off");
-        }
-    }, [selectedTab]);
+  useEffect(() => {
+    if (selectedTab !== menuBarIndex.Skills) {
+      document.getElementById("skills").classList.add("off");
+    } else {
+      document.getElementById("skills").classList.remove("off");
+    }
+  }, [selectedTab]);
 
-    return (
-        <>
-            <div id="skills" className="main-view off">
-                <div className="header">Skills</div>
+  return (
+    <>
+      <div id="skills" className="main-view off">
+        <div className="header">Skills</div>
 
-                <div className="content-container"></div>
-            </div>
-        </>
-    );
+        <div className="content-container">COMING SOON</div>
+      </div>
+    </>
+  );
 }
